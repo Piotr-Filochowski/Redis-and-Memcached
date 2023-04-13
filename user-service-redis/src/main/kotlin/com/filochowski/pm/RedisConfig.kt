af10a945eff1da0ca24464e5048d9818c180d54a
Piotr Filochowski <piotr.filochowski01@gmail.com>
2023-04-13 14:51:00 +0200

    temp 2

--- user-service-redis/src/main/kotlin/com/filochowski/pm/RedisConfig.kt
+++ user-service-redis/src/main/kotlin/com/filochowski/pm/RedisConfig.kt
@@ -13,21 +13,21 @@ class RedisConfig {
     @Bean
     fun connectionFactory(): JedisConnectionFactory {
         val configuration = RedisStandaloneConfiguration()
-        configuration.setHostName("localhost")
-        configuration.setPort(6379)
+        configuration.hostName = "localhost"
+        configuration.port = 6379
         return JedisConnectionFactory(configuration)
     }
 
-    @Bean
-    fun template(): RedisTemplate<String, Any> {
-        val template: RedisTemplate<String, Any> = RedisTemplate()
-        template.setConnectionFactory(connectionFactory())
-        template.setKeySerializer(StringRedisSerializer())
-        template.setHashKeySerializer(StringRedisSerializer())
-        template.setHashKeySerializer(JdkSerializationRedisSerializer())
-        template.setValueSerializer(JdkSerializationRedisSerializer())
-        template.setEnableTransactionSupport(true)
-        template.afterPropertiesSet()
-        return template
-    }
+//    @Bean
+//    fun template(): RedisTemplate<String, Any> {
+//        val template: RedisTemplate<String, Any> = RedisTemplate()
+//        template.setConnectionFactory(connectionFactory())
+//        template.keySerializer = StringRedisSerializer()
+//        template.hashKeySerializer = StringRedisSerializer()
+//        template.hashKeySerializer = JdkSerializationRedisSerializer()
+//        template.valueSerializer = JdkSerializationRedisSerializer()
+//        template.setEnableTransactionSupport(true)
+//        template.afterPropertiesSet()
+//        return template
+//    }
 }
